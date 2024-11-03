da66b50c5b143ddd8c42cae1c6572caf794ff1fb
diegouhh <ju4norteg40@gmail.com>
2024-11-03 01:45:28 -0500

    BET (sin la revisión visual y carpeta derivatives)

--- Prototipo1.sh
+++ Prototipo1.sh
@@ -37,8 +37,8 @@
 # ==================================================================================
 
 COLOR_ERROR="\e[38;5;203m"    # Rojo claro
-COLOR_WARNING="\e[38;5;190m"  # Amarillo claro
-COLOR_SUCCESS="\e[38;5;154m"  # Verde claro
+COLOR_ADVERTENCIA="\e[38;5;190m"  # Amarillo claro
+COLOR_EXITO="\e[38;5;154m"  # Verde claro
 COLOR_MODULO="\e[38;5;99m"    # Divide comandos principales
 COLOR_NORMAL="\e[0m"          # Color normal
 
@@ -53,23 +53,23 @@ echo -e "${COLOR_MODULO}==============================================${COLOR_NO
 # Función que solicita la ruta del archivo en la terminal y verifica si el archivo existe con extensiones .nii o .nii.gz
 solicitar_ruta_terminal_individual(){
     echo -e "Se usará el método alternativo de entrada por terminal."
-    read -e -p "Introduce la ruta del archivo .nii o .nii.gz: " input_image_path
+    read -e -p "Introduce la ruta del archivo .nii o .nii.gz: " ruta_imagen
 
     # Comprobar si el archivo existe con la extensión proporcionada o sin ella
-    if [[ -f "$input_image_path" ]]; then
+    if [[ -f "$ruta_imagen" ]]; then
 
         # El archivo existe con la ruta completa (incluyendo la extensión)
-        echo -e "${COLOR_SUCCESS}\nArchivo encontrado: ${input_image_path##*/}${COLOR_NORMAL}"
-    elif [[ -f "${input_image_path}.nii" ]]; then
+        echo -e "${COLOR_EXITO}\nArchivo encontrado: ${ruta_imagen##*/}${COLOR_NORMAL}"
+    elif [[ -f "${ruta_imagen}.nii" ]]; then
 
         # Añadir extensión .nii si existe
-        input_image_path="${input_image_path}.nii"
-        echo -e "${COLOR_SUCCESS}\nArchivo encontrado: ${input_image_path##*/}${COLOR_NORMAL}"
-    elif [[ -f "${input_image_path}.nii.gz" ]]; then
+        ruta_imagen="${ruta_imagen}.nii"
+        echo -e "${COLOR_EXITO}\nArchivo encontrado: ${ruta_imagen##*/}${COLOR_NORMAL}"
+    elif [[ -f "${ruta_imagen}.nii.gz" ]]; then
 
         # Añadir extensión .nii.gz si existe
-        input_image_path="${input_image_path}.nii.gz"
-        echo -e "${COLOR_SUCCESS}\nArchivo encontrado: ${input_image_path##*/}${COLOR_NORMAL}"
+        ruta_imagen="${ruta_imagen}.nii.gz"
+        echo -e "${COLOR_EXITO}\nArchivo encontrado: ${ruta_imagen##*/}${COLOR_NORMAL}"
     else
         echo -e "${COLOR_ERROR}\nFormato de archivo no reconocido. Debe ser .nii o .nii.gz. Saliendo...${COLOR_NORMAL}"
         exit 1
@@ -79,11 +79,11 @@ solicitar_ruta_terminal_individual(){
 
 solicitar_ruta_terminal_bids(){
     echo -e "Se usará el método de entrada por terminal."
-    read -e -p "Introduce la ruta del directorio BIDS: " directorio_bids
+    read -e -p "Introduce la ruta del directorio BIDS: " ruta_directorio_bids
 
     # Comprobar si el directorio existe.
-    if [[ -d "$directorio_bids" ]]; then
-        echo -e "${COLOR_SUCCESS}\nDirectorio encontrado: ${directorio_bids}${COLOR_NORMAL}"
+    if [[ -d "$ruta_directorio_bids" ]]; then
+        echo -e "${COLOR_EXITO}\nDirectorio encontrado: ${ruta_directorio_bids}${COLOR_NORMAL}"
     else 
         echo -e "${COLOR_ERROR}\nDirectorio no encontrado. Saliendo...${COLOR_NORMAL}"
         exit 1    
@@ -92,47 +92,50 @@ solicitar_ruta_terminal_bids(){
 
 procesar_archivo_bids(){
 
-    echo -e "${COLOR_MODULO}Iniciando procesamiento en paralelo...${COLOR_NORMAL}"
+    echo -e "Iniciando procesamiento en paralelo..."
 
-    # Encontrar archivos *_T1w.nii o *_T1w.nii.gz y procesarlos en paralelo
-    find "$directorio_bids" -type f -path "*/sub-*/anat/*_T1w.nii*" > "$directorio_bids"/imagenes_a_procesar.txt
+    # Encontrar archivos *_T1w.nii o *_T1w.nii.gz y almacenarlos
+    find "$ruta_directorio_bids" -type f -path "*/sub-*/anat/*_T1w.nii*" > "$ruta_directorio_bids"/lista_imagenes_a_procesar_robustfov.txt
 
-    # Quitar las extensiones .nii o .nii.gz de cada linea en imagenes_a_procesar.txt
-    sed -i -E 's/\.(nii|nii\.gz)$//' "$directorio_bids"/imagenes_a_procesar.txt
+    # Quitar las extensiones .nii o .nii.gz de cada linea en lista_imagenes_a_procesar_robustfov.txt
+    sed -i -E 's/\.(nii|nii\.gz)$//' "$ruta_directorio_bids"/lista_imagenes_a_procesar_robustfov.txt
 
     while read line; do
-        output_file="${line}_crop*"
+        archivo_salida="${line}_crop*"
 
         # Si el archivo de salida ya existe, eliminarlo
-        [ -f "$output_file" ] && rm "$output_file"
-    done < "$directorio_bids/imagenes_a_procesar.txt"
-
+        [ -f "$archivo_salida" ] && rm "$archivo_salida"
+    done < "$ruta_directorio_bids/lista_imagenes_a_procesar_robustfov.txt"
 
     # Ejecuta robustfov en paralelo con xargs y 4 procesos
-    cat "$directorio_bids"/imagenes_a_procesar.txt | xargs -P 4 -I {} robustfov -i {} -r "{}_crop.nii.gz"
+    echo -e "${COLOR_MODULO}Iniciando la reducción del FOV (Robustfov)${COLOR_NORMAL}"    
+    cat "$ruta_directorio_bids"/lista_imagenes_a_procesar_robustfov.txt | xargs -P 4 -I {} robustfov -i {} -r "{}_crop.nii.gz"
 
-    # Eliminar el archivo imagenes_a_procesar.txt después de terminar el procesamiento
-    rm "$directorio_bids/imagenes_a_procesar.txt"
+    # Eliminar el archivo lista_imagenes_a_procesar_robustfov.txt después de terminar el procesamiento
+    rm "$ruta_directorio_bids/lista_imagenes_a_procesar_robustfov.txt"
 
-    echo -e "${COLOR_SUCCESS}Recorte de cuello terminado.${COLOR_SUCCESS}"
+    echo -e "${COLOR_EXITO}Reducción del FOV terminada.${COLOR_EXITO}"
 }
 
 while true; do
 # Menú para elegir entre BIDS o archivo individual
 echo -e -n $'¿Deseas procesar un archivo individual o trabajar con un directorio BIDS?\n\n- Archivo individual (i)\n- Directorio BIDS (b)\n\nElige una opción (i/b): '
-read -r opcion
+read -r opcion_entrada
 
-# Procesamiento individual
-if [[ "$opcion" == "i" || "$opcion" == I ]]; then  
+# ==================================================================================
+# Robustfov individual
+# ==================================================================================
+if [[ "$opcion_entrada" == "i" || "$opcion_entrada" == I ]]; then  
+    echo -e "${COLOR_MODULO}Has elegido procesar un archivo individual.${COLOR_NORMAL}"
     # Bucle para repetir el menú hasta que el usuario elija una opción válida
     while true; do
         echo -e -n $'\n¿Cómo deseas ingresar los datos? \n\n- Interfaz gráfica (g)\n- Terminal (t)\n\nElige una opción (g/t): '
-        read -r metodo
+        read -r metodo_entrada
 
-        if [[ "$metodo" == "g" || "$metodo" == "G" ]]; then
+        if [[ "$metodo_entrada" == "g" || "$metodo_entrada" == "G" ]]; then
             # Comprueba si Zenity está instalado, despreciando el output de ´command´
             if ! command -v zenity &> /dev/null; then
-                echo -e "${COLOR_WARNING}\nZenity no está instalado.${COLOR_NORMAL}"
+                echo -e "${COLOR_ADVERTENCIA}\nZenity no está instalado.${COLOR_NORMAL}"
 
                 # Preguntar si se quiere instalar Zenity
                 read -p "Deseas instalar Zenity? (s/n) " instalar_zenity
@@ -163,10 +166,10 @@ if [[ "$opcion" == "i" || "$opcion" == I ]]; then
                             
                             # Si Zenity está instalado, solicitar la ruta del archivo mediante un cuadro de diálogo gráfico
                                 echo -e "Selecciona un archivo .nii o .nii.gz."
-                                input_image_path=$(zenity --file-selection --title="Selecciona un archivo .nii o .nii.gz" --filename="$HOME/" --file-filter="*.nii *.nii.gz" 2>/dev/null)
+                                ruta_imagen=$(zenity --file-selection --title="Selecciona un archivo .nii o .nii.gz" --filename="$HOME/" --file-filter="*.nii *.nii.gz" 2>/dev/null)
 
                                 # Comprobar si se seleccionó un archivo
-                                if [[ -z "$input_image_path" ]]; then
+                                if [[ -z "$ruta_imagen" ]]; then
                                     echo -e "${COLOR_ERROR}No se seleccionó ningún archivo. Saliendo...${COLOR_NORMAL}"
                                     exit 1
                                 fi
@@ -180,17 +183,17 @@ if [[ "$opcion" == "i" || "$opcion" == I ]]; then
             else
                 # Si Zenity está instalado, solicitar la ruta del archivo mediante un cuadro de diálogo gráfico
                 echo -e "Selecciona un archivo .nii o .nii.gz."
-                input_image_path=$(zenity --file-selection --title="Selecciona un archivo .nii o .nii.gz" --filename="$HOME/" --file-filter="*.nii *.nii.gz" 2>/dev/null)
+                ruta_imagen=$(zenity --file-selection --title="Selecciona un archivo .nii o .nii.gz" --filename="$HOME/" --file-filter="*.nii *.nii.gz" 2>/dev/null)
 
                 # Comprobar si se seleccionó un archivo
-                if [[ -z "$input_image_path" ]]; then
+                if [[ -z "$ruta_imagen" ]]; then
                     echo -e "${COLOR_ERROR}No se seleccionó ningún archivo. Saliendo...${COLOR_NORMAL}"
                     exit 1
                 fi
             fi
             break  # Salir del bucle si la opción fue válida
 
-        elif [[ "$metodo" == "t" || "$metodo" == "T" ]]; then
+        elif [[ "$metodo_entrada" == "t" || "$metodo_entrada" == "T" ]]; then
             # Ruta directa vía terminal
             solicitar_ruta_terminal_individual
             break  # Salir del bucle si la opción fue válida
@@ -202,35 +205,38 @@ if [[ "$opcion" == "i" || "$opcion" == I ]]; then
     done
 
     # Definir el nombre base para el archivo recortado
-    crop_base_name="${input_image_path%%.*}_crop.nii.gz"
+    nombre_base_crop="${ruta_imagen%%.*}_crop.nii.gz"
 
     # Comprobar si el archivo de salida ya existe y sobreescribir
-    if [ -f "$crop_base_name" ]; then
-        echo -e "${COLOR_WARNING}\nEl archivo ${crop_base_name##*/} ya existe. Será sobrescrito.\n${COLOR_NORMAL}"
+    if [ -f "$nombre_base_crop" ]; then
+        echo -e "${COLOR_ADVERTENCIA}\nEl archivo ${nombre_base_crop##*/} ya existe. Será sobrescrito.\n${COLOR_NORMAL}"
     else   
-        echo -e "\nEl archivo será guardado como ${crop_base_name##*/}.\n"
+        echo -e "\nEl archivo será guardado como ${nombre_base_crop##*/}.\n"
     fi
 
     # Mensaje de inicio del proceso
     echo -e "Recorte de cuello iniciado...\n"
     # Ejecutar robustfov para realizar la operación
-    robustfov -i "$input_image_path" -r "$crop_base_name"
+    robustfov -i "$ruta_imagen" -r "$nombre_base_crop"
     # Mensaje de finalización
-    echo -e "${COLOR_SUCCESS}Resultado guardado en la ruta ${crop_base_name} el día $(date)${COLOR_NORMAL}"
+    echo -e "${COLOR_EXITO}Resultado guardado en la ruta ${nombre_base_crop} el día $(date)${COLOR_NORMAL}"
     break
 
 # ----------------------------------------------------------------------------------------------------------------------------- #
 
-# Procesamiento en BIDS (ROBUSTFOV)
-elif [[ "$opcion" == "b" || "$opcion" == "B" ]]; then
+# ==================================================================================
+#Robustfov BIDS
+# ==================================================================================
+elif [[ "$opcion_entrada" == "b" || "$opcion_entrada" == "B" ]]; then
+    echo -e "${COLOR_MODULO}Has elegido trabajar con un directorio BIDS.${COLOR_NORMAL}"
     while true; do
             echo -e -n $'\n¿Cómo deseas ingresar los datos? \n\n- Interfaz gráfica (g)\n- Terminal (t)\n\nElige una opción (g/t): '
-            read -r metodo
+            read -r metodo_entrada
 
-            if [[ "$metodo" == "g" || "$metodo" == "G" ]]; then
+            if [[ "$metodo_entrada" == "g" || "$metodo_entrada" == "G" ]]; then
             # Comprueba si Zenity está instalado, despreciando el output de ´command´
             if ! command -v zenity &> /dev/null; then
-                echo -e "${COLOR_WARNING}\nZenity no está instalado.${COLOR_NORMAL}"
+                echo -e "${COLOR_ADVERTENCIA}\nZenity no está instalado.${COLOR_NORMAL}"
 
                 # Preguntar si se quiere instalar Zenity
                 read -p "Deseas instalar Zenity? (s/n) " instalar_zenity
@@ -261,10 +267,10 @@ elif [[ "$opcion" == "b" || "$opcion" == "B" ]]; then
                             
                             # Si Zenity está instalado, solicitar la ruta del archivo mediante un cuadro de diálogo gráfico
                                 echo -e "Selecciona un directorio en estándar BIDS."
-                                directorio_bids=$(zenity --file-selection --directory --title="Selecciona un directorio en estándar BIDS" --filename="$HOME/" 2>/dev/null)
+                                ruta_directorio_bids=$(zenity --file-selection --directory --title="Selecciona un directorio en estándar BIDS" --filename="$HOME/" 2>/dev/null)
 
                                 # Comprobar si se seleccionó un archivo
-                                if [[ -z "$directorio_bids" ]]; then
+                                if [[ -z "$ruta_directorio_bids" ]]; then
                                     echo -e "${COLOR_ERROR}No se seleccionó ningún directorio. Saliendo...${COLOR_NORMAL}"
                                     exit 1
                                 fi
@@ -278,17 +284,17 @@ elif [[ "$opcion" == "b" || "$opcion" == "B" ]]; then
             else
                 # Si Zenity está instalado, solicitar la ruta del archivo mediante un cuadro de diálogo gráfico
                 echo -e "Selecciona un directorio en estándar BIDS."
-                directorio_bids=$(zenity --file-selection --directory --title="Selecciona un directorio en estándar BIDS" --filename="$HOME/" 2>/dev/null)
+                ruta_directorio_bids=$(zenity --file-selection --directory --title="Selecciona un directorio en estándar BIDS" --filename="$HOME/" 2>/dev/null)
 
                 # Comprobar si se seleccionó un directorio
-                if [[ -z "$directorio_bids" ]]; then
+                if [[ -z "$ruta_directorio_bids" ]]; then
                     echo -e "${COLOR_ERROR}No se seleccionó ningún directorio. Saliendo...${COLOR_NORMAL}"
                     exit 1
                 fi
             fi
             break  # Salir del bucle si la opción fue válida (se lleva el nombre del directorio)
 
-        elif [[ "$metodo" == "t" || "$metodo" == "T" ]]; then
+        elif [[ "$metodo_entrada" == "t" || "$metodo_entrada" == "T" ]]; then
             # Ruta directa vía terminal
             solicitar_ruta_terminal_bids
             break  # Salir del bucle si la opción fue válida
@@ -308,7 +314,51 @@ fi
 done
 
 # Anuncio de inicio del BET
-echo -e "${COLOR_MODULO}\nIniciando BET (Brain Extraction Tool)...\n${COLOR_NORMAL}"
+echo -e "${COLOR_MODULO}\nIniciando BET (Brain Extraction Tool).\n${COLOR_NORMAL}"
 
-# Procesamiento individual (ROBUSTFOV)
+# Verifica si nombre_base_crop está definido y no vacío (si no está vacío indica que se siguió la ruta individual.)
+
+# ==================================================================================
+# BET individual
+# ==================================================================================
+if [ ! -z "$nombre_base_crop" ]; then
+    # Define la ruta de salida para el archivo de salida del BET, eliminando la extensión
+    nombre_salida_bet="${nombre_base_crop%%.*}_brain.nii.gz"
+    echo "Procesando BET en ruta individual para: $nombre_base_crop"
+    bet "$nombre_base_crop" "$nombre_salida_bet" -f 0.5
+    # Mensaje indicando que el proceso en ruta individual ha finalizado
+    echo -e "${COLOR_EXITO}Proceso BET completado para: $nombre_base_crop.${COLOR_NORMAL}"
+
+else 
+# ==================================================================================
+# BET BIDS
+# ==================================================================================
+    # Busca todos los archivos *_crop.nii en el directorio BIDS y los guarda en un archivo de lista
+    find "$ruta_directorio_bids" -type f -path "*/sub-*/anat/*_crop.nii*" > "$ruta_directorio_bids"/lista_imagenes_a_procesar_bet.txt
+    # Quita las extensiones .nii o .nii.gz de cada linea en lista_imagenes_a_procesar_bet.txt
+    sed -i -E 's/\.(nii|nii\.gz)$//' "$ruta_directorio_bids"/lista_imagenes_a_procesar_bet.txt
+    
+
+    # Revisa cada línea de la lista para determinar si los archivos de salida existen
+    while read line; do
+        archivo_salida_bet="${line}_brain*"
+
+        # Si el archivo de salida ya existe, eliminarlo
+        [ -f "$archivo_salida_bet" ] && rm "$archivo_salida_bet"
+    done < "$ruta_directorio_bids/lista_imagenes_a_procesar_bet.txt"
+
+
+    # Mensaje indicando que el procesamiento se ha iniciado
+    echo "Procesamiento BET en curso..."
+
+    # Ejecuta el comando BET en paralelo
+    cat "$ruta_directorio_bids"/lista_imagenes_a_procesar_bet.txt | xargs -P 5 -I {} bet {} "{}_brain.nii.gz"
+
+    # Eliminar el archivo lista_imagenes_a_procesar_bet.txt después de terminar el procesamiento
+    rm "$ruta_directorio_bids/lista_imagenes_a_procesar_bet.txt"
+
+# Mensaje indicando que el proceso ha finalizado
+    echo -e "${COLOR_EXITO}Extracción de cerebro terminada para todos los archivos.${COLOR_EXITO}"
+
+fi
 
